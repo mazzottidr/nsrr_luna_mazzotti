344b3bc8ffd8371e8f8b0cb578a60ea3f0135f06
Kent, Alexander M <akent@rics.bwh.harvard.edu>
2021-02-09 10:24:46 -0500

    Updated twineannots.sh with more comments

--- studies/wsc/twin/twineannots.sh
+++ studies/wsc/twin/twineannots.sh
@@ -5,7 +5,7 @@ DIR=/data/nsrr/working/wsc-scoring-annotations
 for f in ${DIR}/twin/*allScore.txt
 do
 id=`cut -d'/' -f7 <<< $f | cut -d'.' -f1 | awk '{print substr($1,1, length($1)-8)}'` #get id for each file 
-awk -F"\t" ' { print $2 } ' ${DIR}/twin/${id}allScore.txt | sort | uniq -c
+awk -F"\t" ' { print $2 } ' ${DIR}/twin/${id}allScore.txt | sort | uniq -c #extract staging to temporary .txt file
 tr -d '\r' < ${DIR}/twin/${id}allScore.txt | awk -F"\t" \
     ' BEGIN { printf "# N1\n# N2\n# N3\n# REM\n# wake\n# ?\n" } \
       $2 == "START RECORDING" { t = $1 ; s = "" } \
@@ -22,7 +22,7 @@ tr -d '\r' < ${DIR}/twin/${id}allScore.txt | awk -F"\t" \
       $2 == "STAGE - R" && s == "" { s = "REM" ; t = $1 } \
       $2 == "STAGE - W" && s == "" { s = "wake" ; t = $1 } \
       $2 == "STAGE - NO STAGE" && s == "" { s = "?" ; t = $1 } '  OFS="\t" > tmp.txt
-sed 's/\.[0-9][0-9]//g' < tmp.txt > tmp2.annot #create temporary .annot file 
-luna ${DIR}/twin/${id}.edf annot-file=tmp2.annot -o out.db -s STAGE 
-destrat out.db +STAGE -r E -v STAGE | awk ' NR != 1 { print $3 } ' > ${id}.eannot #create final .eannot file
+sed 's/\.[0-9][0-9]//g' < tmp.txt > tmp2.annot #create temporary .annot file from temporary .txt file
+luna ${DIR}/twin/${id}.edf annot-file=tmp2.annot -o out.db -s STAGE #run luna's STAGE command on temporary .annot file
+destrat out.db +STAGE -r E -v STAGE | awk ' NR != 1 { print $3 } ' > ${id}.eannot #create final .eannot file from luna output
 done
